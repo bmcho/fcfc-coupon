ab277d469fe5efe4abfaeaca5cb786f4a2f5c88f
bmcho <bmcho91@naver.com>
2025-05-28 17:52:51 +0900

    fix: 상위 모듈 core dependencies 참조 못하는 에러수정

--- coupon-api/build.gradle.kts
+++ coupon-api/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation(project(":coupon-core"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
+
 }
 
 tasks.withType<Test> {
--- coupon-core/build.gradle.kts
+++ coupon-core/build.gradle.kts
@@ -1,12 +1,20 @@
 val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
 bootJar.enabled = false
 
+val jar: Jar by tasks
+jar.enabled = true
+
 repositories {
     mavenCentral()
 }
 
-dependencies {
+plugins {
+    `java-library`
+}
 
+dependencies {
+    api("org.springframework.boot:spring-boot-starter-web")
+    api("org.redisson:redisson-spring-boot-starter:3.47.0")
 }
 
 tasks.withType<Test> {
